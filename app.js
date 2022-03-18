cd7bcea5f6441931310852d17f7c212bd3d120b1
BADIDDO <aymenmatrix311@gmail.com>
2022-03-18 15:24:03 +0100

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".btn");
+const body = document.querySelector("body");
+
+btn.addEventListener("click" , () => {
+    body.style.backgroundImage = "linear-gradient( 135deg, #3c2c2c  10%, #592a2a 100%)";
+})
